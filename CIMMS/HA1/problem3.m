4b4fd8655ca1f6e5ec88e9b8c3e27b755f27a729
brunoisy <degomme.bruno@gmail.com>
2017-04-09 20:03:07 +0200

    plop

--- CIMMS/HA1/problem3.m
+++ CIMMS/HA1/problem3.m
@@ -6,13 +6,13 @@ N = 50; % #particles
 m = 2;
 
 X = drawInitPart(N);
-w = weighPart(X);
+w = weighPart(X, Y(:,1));
 
 tau1 = zeros(1,m);
-tau2 = zeros(2,m);
-for n=1:m-1
+tau2 = zeros(1,m);
+for n=2:m+1
     X = updatePart(X);
-    w = weighPart(X);
+    w = weighPart(X, Y(:,n));
     
     tau1(n) = sum(X(1,:).*w)/sum(w);
     tau2(n) = sum(X(4,:).*w)/sum(w);
--- CIMMS/HA1/weighPart.m
+++ CIMMS/HA1/weighPart.m
@@ -1,13 +1,20 @@
-function [ w] = weighPart( X )
+function [w] = weighPart(X, y)
 
-load('RSSI-measurements')
 s = size(X);
 N = s(2);
 
 w = zeros(1,N);
-for i=1:N %vectorise
-    w(i) = p(Y(:,1), X(:,i)); % doesn't work for now!!!
+
+mu = zeros(6,N);
+for i=1:N    
+    for l=1:6
+        x = [X(1,i);X(4,i)];
+        mu(l,i) = v-10*eta*log10(norm(x-pos_vec(l)));
+    end
+    sigmaInv = eye(6)*zeta^(-1);
+    w(i) = 1/(sqrt(2*pi*zeta))*e^(-1/2*(y-mu(l,i))'*sigmaInv*(y-mu(l,i)));
 end
+
 
 end
 
