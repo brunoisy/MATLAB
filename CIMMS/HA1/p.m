3232fb9708f6f2a55ca4f01db22fb13752105f15
brunoisy <degomme.bruno@gmail.com>
2017-04-10 12:59:05 +0200

    basic algo works'

--- CIMMS/HA1/p.m
+++ CIMMS/HA1/p.m
@@ -1,13 +1,26 @@
-function out = p(y, x)
+function [w] = p(part, y)
+
+s = size(part);
+N = s(2);
+
 load('stations')
 v = 90;
 eta = 3;
 zeta = 1.5;
-mu = zeros(6,1);
+
+mu = zeros(6,N);
 for l=1:6
-    mu(l) = v-10*eta*log10(norm([x(1);x(4)]-pos_vec(:,l)));
+    x = [part(1,:);part(4,:)];
+    A = x-repmat(pos_vec(:,l),1,N);
+    norms = sqrt(A(1,:).^2+A(2,:).^2);
+    mu(l,:) = v-10*eta*log10(norms);
 end
-out = mvnpdf(y, mu, eye(6,6)*zeta);
+B = repmat(y,1,N)-mu;
+C = zeros(1,N);
+for l=1:6
+    C = C + B(l,:).^2;
+end
+w = 1/sqrt(2*pi*zeta)*exp(-1/2*zeta^(-1)*C);
 
 end
 
--- CIMMS/HA1/problem3.m
+++ CIMMS/HA1/problem3.m
@@ -3,7 +3,7 @@ load('stations')
 rng(3)
 
 N = 50; % #particles
-m = 2;
+m = 5;
 
 X = drawInitPart(N);
 w = weighPart(X, Y(:,1));
@@ -12,7 +12,7 @@ tau1 = zeros(1,m);
 tau2 = zeros(1,m);
 for n=2:m+1
     X = updatePart(X);
-    w = weighPart(X, Y(:,n));
+    w = w.*p(X, Y(:,n));
     
     tau1(n) = sum(X(1,:).*w)/sum(w);
     tau2(n) = sum(X(4,:).*w)/sum(w);
