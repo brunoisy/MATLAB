01c25da6d94fe15eb6c6b675e5ceb871bf9ce73c
brunoisy <degomme.bruno@gmail.com>
2017-10-09 11:49:22 +0200

    plop

--- Thesis/fd_curve.m
+++ Thesis/fd_curve.m
@@ -2,9 +2,9 @@ function [ F ] = fd_curve(Lc, X)
 %FD_CURVE Summary of this function goes here
 %   Detailed explanation goes here
 
-global kb T lp
+global kb T lp C
 
-Fx = @(x) -kb*T/lp*(1/(4*(1-x/Lc)^2)-1/4+x/Lc);
+Fx = @(x) -C*(1/(4*(1-x/Lc)^2)-1/4+x/Lc);
 F = zeros(1,length(X));
 for i = 1:length(X)
     F(i) = Fx(X(i));
